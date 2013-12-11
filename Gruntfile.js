78297e831fda80fc64a1d8f5effbf8d936753711
jasonmelgoza <jason@rightscale.com>
2013-12-10 17:45:31 -0800

    added grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,1 +1,36 @@
-Gruntfile.js
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'app/stylesheets/style.css': 'scss/style.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: 'app'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('server', ['connect','watch']);
+}
